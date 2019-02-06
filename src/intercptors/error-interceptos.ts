58743be7d1fddb014dd8348a0b6a9acbc82c22af
rodrigo <alssuffi@yahoo.com.br>
2019-02-06 11:54:10 -0200

    Tratamento padrao para outros erros com Alert

--- src/intercptors/error-interceptos.ts
+++ src/intercptors/error-interceptos.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { StorageService } from 'src/services/storage.service';
+import { AlertController } from '@ionic/angular';
+import { Component } from '@angular/core';
+
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService,
+                public alertController: AlertController){
 
     }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -25,7 +29,15 @@ export class ErrorInterceptor implements HttpInterceptor {
                 switch(errorObj.status){
                     case 403:
                     this.handle403();
+                    this.handle401();
                     break;
+                    case 401:
+                    this.handle401();
+                    break;
+                    default:
+                    this.handleDefault(errorObj);
+                    break;
+
                 }
             }
             return Observable.throw(errorObj)
@@ -35,6 +47,29 @@ export class ErrorInterceptor implements HttpInterceptor {
     handle403(){
         this.storage.setLocalUser(null);
     }
+    
+    async handle401(){
+            const alert = await this.alertController.create({
+              header: 'Alert',
+              subHeader: 'Erro 401: falha de autenticação',
+              message: 'E-mail ou Senha incorretos',
+              buttons: ['OK']
+            });
+            await alert.present();
+            this.storage.setLocalUser(null);
+          }
+    
+    async  handleDefault(errorObj){
+        const alert = await this.alertController.create({
+            header: 'Alert',
+            subHeader: 'Erro ' + errorObj,
+            message: 'Erro generico' + errorObj,
+            buttons: ['OK']
+          });
+          await alert.present();
+          this.storage.setLocalUser(null);
+    }
+    
 }
 
 export const ErrorInterceptorProvider = {
